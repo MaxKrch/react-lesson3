fbd62b22387d79fcfe97f82097d6bd1d5ac09f6c
Max Kyrchanov <max.kyrchanov@gmail.com>
2025-06-20 20:27:32 +0500

    complete config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
+ 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { name } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${name}/`,
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 })
